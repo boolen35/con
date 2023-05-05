1e19bea98e0d46c4a143b8c3ae85acfb73d18fa6
boolen35 <122870851+boolen35@users.noreply.github.com>
2023-05-05 23:12:50 +0800

    Update foc.js

--- foc.js
+++ foc.js
@@ -18,7 +18,7 @@
 [rewrite_local]
   
 # > focos☆解锁会员权限（2023-05-05）@ddgksf2013
-^https?:\/\/focos\.oracle\.bendingspoonsapps\.com\/v\d\/(users\/setup|purchases\/verify\/apple) url script-response-body foc.js 
+^https?:\/\/focos\.oracle\.bendingspoonsapps\.com\/v\d\/(users\/setup|purchases\/verify\/apple) url script-response-body https://raw.githubusercontent.com/boolen35/con/main/foc.js
 
 
 [mitm]
